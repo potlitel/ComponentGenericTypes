5b7287484b440b2a68854b4513f6810a318b70b0
potlitel <potlitel@gmail.com>
2023-11-01 12:14:59 -0400

    Add project files.

---
+++ Data/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ComponentGenericTypes.Data
+{
+    public class Person
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
